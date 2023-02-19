de01fb6e922c4ec17816dfaf75a0851fe4fc6767
mama4294 <matthew.malone@colorado.edu>
2023-02-19 16:32:11 -0700

    Adds metadata

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,16 @@ export default function Head() {
   return (
     <>
       <title>MattGPT</title>
+      <meta property="og:title" content="MattGPT" key="title" />
+      <meta
+        name="description"
+        content="A natural language processing tool driven by AI technology"
+      ></meta>
+      <meta name="author" content="Matthew Malone" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="language" content="English"></meta>
+      <meta property="og:image" content={"/mattGPThomescreen.png"} />
+      <meta property="twitter:image" content={"/mattGPThomescreen.png"} />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
