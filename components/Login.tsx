077e03ce7d8184af994f2dbca2f02c3f2f1a1dfe
mama4294 <matthew.malone@colorado.edu>
2023-02-19 16:17:16 -0700

    Changes login screen

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,23 +2,30 @@
 
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import headshot from "../public/Headshot.jpg";
 
 // whitelist localhost:3000 and domain name at https://console.cloud.google.com/apis/credentials?project=mattgpt-498f2
 
 function Login() {
   return (
-    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
-      <Image
-        src="https://links.papareact.com/2i6"
+    <div className="bg-gradient-to-r from-[#11A37F] to-[#0A614C] h-screen flex flex-col items-center justify-center text-center text-white">
+      {/* <Image
+        // src="https://links.papareact.com/2i6"
+        src={headshot}
+        className="rounded-full"
         alt="Logo"
         width={300}
         height={300}
-      />
+      /> */}
+      <h1 className="font-bold text-4xl mb-2">MattGPT</h1>
+      <p className="text-lg mb-10">
+        A natural language processing tool driven by AI technology{" "}
+      </p>
       <button
-        className="text-white font-bold text-3xl animate-pulse"
+        className="text-2xl border border-white rounded-full p-4 hover:bg-white hover:text-[#11A37F]"
         onClick={() => signIn("google")}
       >
-        Sign in to use MattGPT
+        Sign in
       </button>
     </div>
   );
