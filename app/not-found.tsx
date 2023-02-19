c0febea5ba534e22cc31a5c9c266c0a670b20487
mama4294 <matthew.malone@colorado.edu>
2023-02-19 16:39:20 -0700

    Adds not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center text-white">
+      <p className="font-bold text-4xl mb-2">404</p>
+      <p className="text-lg">This page could not be found.</p>
+    </div>
+  );
+}
+
+export default NotFound;
