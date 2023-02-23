2ae84f6751ccf8338fcfb2d178a54ecd61686375
mama4294 <matthew.malone@colorado.edu>
2023-02-22 18:24:32 -0700

    Adds start conversation on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,23 @@ import {
   SunIcon,
   ExclamationTriangleIcon,
   BoltIcon,
+  PlusIcon,
 } from "@heroicons/react/24/outline";
+import NewChat from "../components/NewChat";
 
 function HomePage() {
   return (
     <div className="h-full overflow-hidden">
       <div className="text-white flex flex-col items-center md:justify-center h-full px-2 overflow-y-auto overflow-x-hidden">
-        <h1 className="text-5xl font-bold my-20">MattGPT</h1>
+        <h1 className="text-5xl font-bold mt-20 mb-5">MattGPT</h1>
+        <div className="mb-20">
+          <NewChat>
+            <div className="border-gray-500 border chatRow m-2">
+              <PlusIcon className="h-4 w-4" />
+              <p>Start Conversation</p>
+            </div>
+          </NewChat>
+        </div>
         <div className="flex flex-col space-x-2 text-center md:flex-row">
           <div className="mb-8">
             <div className="flex flex-col items-center justify-center mb-5">
@@ -17,12 +27,14 @@ function HomePage() {
             </div>
             <div className="space-y-2">
               <p className="infoText">
-                Explain quantum physics to me like a 5th grader
+                "Why is Matthew Malone such a cool guy?"
               </p>
               <p className="infoText">
-                What is the difference between their and there?
+                "Explain quantum computing in simple terms"
               </p>
-              <p className="infoText">Why is Matthew Malone such a cool guy?</p>
+              <p className="infoText">
+                "How do I make an HTTP request in Javascript?"
+              </p>
             </div>
           </div>
           <div className="mb-8">
@@ -49,10 +61,10 @@ function HomePage() {
             </div>
             <div className="space-y-2">
               <p className="infoText">
-                May occasionally generate incorrect information
+                Cannot read your significant other's mind
               </p>
               <p className="infoText">
-                Cannot read your significant other's mind
+                May occasionally generate incorrect information
               </p>
               <p className="infoText">
                 Limited knowledge of world and events after 2021
