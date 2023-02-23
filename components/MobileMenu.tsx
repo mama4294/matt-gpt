869ce901a36aef45c4a05d88d86f57f8b8e45dc0
mama4294 <matthew.malone@colorado.edu>
2023-02-22 20:49:51 -0700

    Adds home button

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import { Bars3Icon, PlusIcon } from "@heroicons/react/24/solid";
 import { useMenuContext } from "../context/MobileMenu";
 import Drawer from "./Drawer";
@@ -8,6 +8,11 @@ import Sidebar from "./Sidebar";
 
 function MobileMenu() {
   const { isMenuOpen, setMenuOpen } = useMenuContext();
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push(`/`);
+  };
 
   return (
     <>
@@ -15,7 +20,12 @@ function MobileMenu() {
         <button className="btn" onClick={() => setMenuOpen(true)}>
           <Bars3Icon className="w-8 h-8 m-2" />
         </button>
-        <p>MattGPT</p>
+        <button
+          onClick={goHome}
+          className="text-gray-300 rounded-lg p-4 hover:bg-gray-700/70 transition-all duration-200 ease-out"
+        >
+          MattGPT
+        </button>
         <NewChat>
           <button className="btn">
             <PlusIcon className="w-8 h-8 m-2" />
