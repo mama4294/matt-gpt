b53c3fe75bccbb954b000b3d666b119868e01afa
mama4294 <matthew.malone@colorado.edu>
2023-02-22 20:44:11 -0700

    adds auto scroll to bottom

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,6 +3,7 @@
 import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { useEffect, useRef } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 import Message from "./Message";
@@ -13,6 +14,7 @@ type Props = {
 
 function Chat({ chatId }: Props) {
   const { data: session } = useSession();
+  const scrollToRef = useRef<null | HTMLDivElement>(null);
   const [messages] = useCollection(
     session &&
       query(
@@ -28,6 +30,11 @@ function Chat({ chatId }: Props) {
       )
   );
 
+  useEffect(() => {
+    // 👇️ scroll to bottom every time messages change
+    scrollToRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className="flex-1 overflow-y-auto overflow-x-hidden">
       {messages?.empty && (
@@ -42,6 +49,7 @@ function Chat({ chatId }: Props) {
       {messages?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
       ))}
+      <div ref={scrollToRef} />
     </div>
   );
 }
